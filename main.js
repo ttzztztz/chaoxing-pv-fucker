2590946000bdbd2322d74642d4e651f0124a7a3f
ttzztztz <ttzztztz@outlook.com>
2021-03-11 21:19:36 +0800

    fix: bugs

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const fetch = require("node-fetch");
 const { JSDOM } = require("jsdom");
 const fs = require("fs/promises");
-const sto = require('timers/promises');
+const sto = require("timers/promises");
 
 const EXEC_TIMES = 60;
 
@@ -13,13 +13,13 @@ const send_pv_req = async ({ cookie, config }, chaperid) => {
   const txt = await resp.text();
   console.log(`chapterid=${chaperid}, resp=${txt}`);
 
-  return txt === 'success'
+  return txt === `'success'`;
 };
 
 const chapterid_list = [];
 const getTeacherAjax = (_courseid, _classid, chaperid) => {
   chapterid_list.push(chaperid);
-}
+};
 
 const menu = async ({ cookie, config }) => {
   const url = `https://mooc1-2.chaoxing.com/mycourse/studentstudycourselist?courseId=${config.courseId}&clazzid=${config.classId}`;
@@ -43,7 +43,10 @@ const menu = async ({ cookie, config }) => {
   console.log(chapterid_list);
 
   for (let i = 0; i < EXEC_TIMES; i++) {
-    const res = await send_pv_req(ctx, chapterid_list[i % chapterid_list.length]);
+    const res = await send_pv_req(
+      ctx,
+      chapterid_list[i % chapterid_list.length]
+    );
     if (!res) {
       process.exit(-1);
     }
@@ -52,4 +55,3 @@ const menu = async ({ cookie, config }) => {
     console.log(`Done #${i}`);
   }
 })();
-
